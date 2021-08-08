2d69b4398619ac90e96e1fa4956278eb11f323ee
henriquecesp <henriquecesps4@gmail.com>
2021-08-08 20:03:40 -0300

    ✨ record

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,9 +1,52 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Client } from '@notionhq/client'
 
-export default function handler(
+const notion = new Client({
+  auth: process.env.NOTION_API_KEY
+})
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).send('Hello World');
+  const { name, email, message } = req.body
+  await notion.pages.create({
+    parent: {
+      database_id: process.env.NOTION_DATABASE_ID || '',
+    },
+    properties: {
+      name: {
+        type: "title",
+        title: [
+          {
+            type: 'text',
+            text: {
+              content: name
+            }
+          }
+        ]
+      },
+      email: {
+        type: 'email',
+        email: email
+      },
+      message: {
+        type: 'rich_text',
+        rich_text: [
+          {
+            type: 'text',
+            text: {
+              content: message
+            }
+          }
+        ]
+      }
+    }
+  }).then(() => {
+    res.status(201).json({ message: 'Sucesso!' })
+  }).catch((error) => {
+    console.log(error)
+    res.status(500).json({ message: 'Ops, algo deu errado!' })
+  })
 }
